aa74e11da4fd146bf3a1d508988f2639996d822a
Dylan <dcren2010@gmail.com>
2021-06-05 13:03:30 -0400

    setup server side socket.io

--- server.js
+++ server.js
@@ -14,14 +14,25 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	res.send('Server is running')
-})
+	res.send('Server is running');
+});
 
+// Build up io connection
+io.on('connection', (socket) => {
+	socket.emit('me', socket.id); // Provide self id to frontend
 
+	socket.on('disconnect', () => {
+		socket.broadcast.emit('callEnded');
+	});
 
+	socket.on('callUser', ({ userToCall, signalData, form, name }) => {
+		io.to(userToCall).emit('callUser', { signal: signalData, form, name });
+	});
 
-
-
+	socket.on('answerCall', (data) => {
+		io.to(data.to).emit('callAccepted', data.signal);
+	});
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
